e85803bf01d9ba63048c9c27644fffb9020ccb2c
Katherina1234 <katherina0799@gmail.com>
2018-09-22 16:30:21 +0300

    sec

--- editor/editor/Form1.cs
+++ editor/editor/Form1.cs
@@ -3,6 +3,7 @@ using System.Collections.Generic;
 using System.ComponentModel;
 using System.Data;
 using System.Drawing;
+using System.Drawing.Drawing2D;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
@@ -12,9 +13,16 @@ namespace editor
 {
     public partial class Form1 : Form
     {
+        Color CurrentColor = Color.Black;
+        bool isPressed = false;
+        Point CurrentPoint;
+        Point PrevPoint;
+        Graphics g;
+        PaintEventArgs f;
         public Form1()
         {
             InitializeComponent();
+            g = panel1.CreateGraphics();
         }
 
         private void iNFOToolStripMenuItem_Click(object sender, EventArgs e)
@@ -36,6 +44,12 @@ namespace editor
             {
                 button6.BackColor = button5.BackColor;
                 button5.BackColor = MyDialog.Color;
+                
+                
+            }
+            DialogResult D = MyDialog.ShowDialog();
+            if (D == System.Windows.Forms.DialogResult.OK) {
+                CurrentColor = MyDialog.Color;
             }
         }
 
@@ -84,6 +98,43 @@ namespace editor
                 button4.BackColor = MyDialog.Color;
                 panel1.BackColor = MyDialog.Color;
             }
+        }
+
+       
+
+      
+        private void for_paint() {
+            Pen p = new Pen(CurrentColor);
+            p.LineJoin = System.Drawing.Drawing2D.LineJoin.Bevel;
+
+            g.DrawLine(p, PrevPoint, CurrentPoint);
+        }
+        private void for_paint1() {
+            Brush l = new SolidBrush(Color.Beige);
+            Brush solidFunnyOrangyBrownBrush = new SolidBrush(Color.FromArgb(255, 155, 100));
+            
+        }
+       
+
+        private void panel1_MouseMove(object sender, MouseEventArgs e)
+        {
+            if (isPressed) {
+                PrevPoint = CurrentPoint;
+                CurrentPoint = e.Location;
+                for_paint();
+            }
+        }
+
+        private void panel1_MouseDown(object sender, MouseEventArgs e)
+        {
+            isPressed = true;
+            CurrentPoint = e.Location;
+
+        }
+
+        private void panel1_MouseUp(object sender, MouseEventArgs e)
+        {
+            isPressed = false;
         }
     }
 }
